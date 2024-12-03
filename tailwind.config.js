23ffa2176b25a046a38702e27117a4c7ba39a54d
Vincent-Valentino <valentinolubu2@gmail.com>
2024-12-03 13:13:58 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",               // Include HTML files
+    "./src/**/*.{js,ts,jsx,tsx}",  // Include all .js, .ts, .jsx, and .tsx files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

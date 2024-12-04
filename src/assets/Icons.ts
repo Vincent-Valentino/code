77d924d5dbcede8560a1f7c1b018ae0f00edbf9b
Vincent-Valentino <valentinolubu2@gmail.com>
2024-12-04 13:23:14 +0700

    sidebar design

---
+++ src/assets/Icons.ts
@@ -0,0 +1,6 @@
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { LuSquareActivity } from "react-icons/lu";
+import { FaCode } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
+export { MdOutlineDashboardCustomize, LuSquareActivity, FaCode, CgProfile }

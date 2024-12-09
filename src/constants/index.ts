276f88c3a89a841d0d71db8731d8249964a85624
egipsinargo <egipsinargo123@gmail.com>
2024-12-09 15:14:46 +0700

    contents for nav menu

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+export const navigation = [
+  {
+    id: 0,
+    title: "Roadmap",
+    url: "#roadmap",
+  },
+  {
+    id: 1,
+    title: "Pricing",
+    url: "#pricing",
+  },
+  {
+    id: 2,
+    title: "Program",
+    url: "#event",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "#blog",
+  }
+];
+
+export const getIn = [
+  {
+    id: 0,
+    title: "Sign In",
+    url: "#login",
+  },
+  {
+    id: 1,
+    title: "Sign Up",
+    url: "#signup",
+  }
+];

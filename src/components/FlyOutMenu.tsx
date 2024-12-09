8af7efed4e6384384548cb72fd56b701b11c1db8
egipsinargo <egipsinargo123@gmail.com>
2024-12-09 15:15:19 +0700

    flyout menu when hover on navbar

---
+++ src/components/FlyOutMenu.tsx
@@ -0,0 +1,17 @@
+const FlyOutMenu = () => {
+  return (
+    <ul className="bg-white shadow-lg border border-gray-200 rounded-lg inline-block whitespace-nowrap">
+      <li className="nunito text-[15px] font-normal px-4 py-3 hover:bg-neutral-100 cursor-pointer">
+        Coder Bootcamp Batch 1
+      </li>
+      <li className="nunito text-[15px] font-normal px-4 py-3 hover:bg-neutral-100 cursor-pointer">
+        Go Camp 2025
+      </li>
+      <li className="nunito text-[15px] font-normal px-4 py-3 hover:bg-neutral-100 cursor-pointer">
+        React Together with DIU
+      </li>
+    </ul>
+  );
+};
+
+export default FlyOutMenu;
